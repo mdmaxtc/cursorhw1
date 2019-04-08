572bc495bda5c223e191a34372ef77e4be72e614
Pasko Serhii <paskosergiy@gmail.com>
2019-04-08 21:47:13 +0300

    added docstrings

--- homework.py
+++ homework.py
@@ -11,6 +11,10 @@ class OurAwesomeException(Exception):
 
 
 def is_two_object_has_same_value(first: Any, second: Any) -> bool:
+    """
+    If @first and @second has the same value should return True
+    In another case should return False
+    """
     if first == second:
         return True
     else:
@@ -18,6 +22,10 @@ def is_two_object_has_same_value(first: Any, second: Any) -> bool:
 
 
 def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
+    """
+    If @first and @second has the same type should return True
+    In another case should return False
+    """
     if type(first) == type(second):
         return True
     else:
@@ -25,6 +33,10 @@ def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
 
 
 def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
+    """
+    If @first and @second are the same object should return True
+    In another case should return False
+    """
     if first is second:
         return True
     else:
@@ -32,6 +44,19 @@ def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
 
 
 def multiple_ints(first_value: int, second_value: int) -> int:
+    """
+    Should calculate product of all args.
+    if first_value or second_value is not int should raise ValueError
+
+    Raises:
+        ValueError
+
+    Params:
+        first_value: value for multiply
+        second_value
+    Returns:
+        Product of elements
+    """
     if type(first_value) != int or type(second_value) != int:
         raise ValueError
     else:
@@ -39,6 +64,32 @@ def multiple_ints(first_value: int, second_value: int) -> int:
 
 
 def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
+    """
+    If possible to convert arguments to int value - convert and multiply them.
+    If it is impossible raise OurAwesomeException
+
+    Args:
+        first_value: number for multiply
+        second_value: number for multiply
+
+    Raises:
+        OurAwesomeException
+
+    Returns: multiple of two numbers.
+
+    Examples:
+        multiple_ints_with_conversion(6, 6)
+        >>> 36
+        multiple_ints_with_conversion(2, 2.0)
+        >>> 4
+        multiple_ints_with_conversion("12", 1)
+        >>> 12
+        try:
+            multiple_ints_with_conversion("Hello", 2)
+        except ValueError:
+            print("Not valid input data")
+        >>> "Not valid input data"
+    """
     first_value = int(first_value)
     second_value = int(second_value)
     if type(first_value * second_value) == int:
@@ -48,6 +99,21 @@ def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
 
 
 def is_word_in_text(word: str, text: str) -> bool:
+    """
+    If text contain word return True
+    In another case return False.
+    
+    Args:
+        word: Searchable substring
+        text: Text for searching
+
+    Examples:
+        is_word_in_text("Hello", "Hello word")
+        >>> True
+        is_word_in_text("Glad", "Nice to meet you ")
+        >>> False
+        
+    """
     if word in text:
         return True
     else:
@@ -55,6 +121,9 @@ def is_word_in_text(word: str, text: str) -> bool:
 
 
 def some_loop_exercise() -> list:
+    """
+    Use loop to create list contains int values from 0 to 12 except 6 and 7
+    """
     list = []
     i = 0
     while i <= 12:
@@ -66,6 +135,14 @@ def some_loop_exercise() -> list:
 
 
 def remove_from_list_all_negative_numbers(data: List[int]) -> list:
+    """
+    Use loops to solve this task.
+    You could use data.remove(negative_number) to solve this issue.
+    Also you could create new list with only positive numbers.
+    Examples:
+        remove_from_list_all_negative_numbers([1, 5, -7, 8, -1])
+        >>> [1, 5, 8]
+    """
     new_lst = data[:]
     for item in new_lst:
         if item < 0:
@@ -74,6 +151,13 @@ def remove_from_list_all_negative_numbers(data: List[int]) -> list:
 
 
 def alphabet() -> dict:
+    """
+    Create dict where keys are alphabetic characters. And values are it number in alphabet
+    Notes You could see the implementaion of this one in test, but create another one
+    Examples:
+        alphabet()
+        >>> {"a": 1, "b": 2 ...}
+    """
     dict = {}
     keys = range(1, 26)
     values = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
@@ -85,13 +169,6 @@ def alphabet() -> dict:
 
 
 def simple_sort(data: List[int]) -> List[list]:
-    for i in range(len(data)):
-        min = i
-        for k in range(i + 1, len(data)):
-            if data[k] < data[min]:
-                min = k
-        data[min], data[i] = data[i], data[min]
-    return data
     """
     Sort list of ints without using built-in methods.
     Examples:
@@ -100,3 +177,10 @@ def simple_sort(data: List[int]) -> List[list]:
     Returns:
 
     """
+    for i in range(len(data)):
+        min = i
+        for k in range(i + 1, len(data)):
+            if data[k] < data[min]:
+                min = k
+        data[min], data[i] = data[i], data[min]
+    return data
