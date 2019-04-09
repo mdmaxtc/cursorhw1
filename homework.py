3874754bbbc22d1371a585288ac7cdc5fb39f668
Pasko Serhii <paskosergiy@gmail.com>
2019-04-09 10:30:48 +0300

    refactored after Viach B reviewed.

--- homework.py
+++ homework.py
@@ -15,10 +15,7 @@ def is_two_object_has_same_value(first: Any, second: Any) -> bool:
     If @first and @second has the same value should return True
     In another case should return False
     """
-    if first == second:
-        return True
-    else:
-        return False
+    return first == second
 
 
 def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
@@ -26,10 +23,7 @@ def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
     If @first and @second has the same type should return True
     In another case should return False
     """
-    if type(first) == type(second):
-        return True
-    else:
-        return False
+    return type(first) == type(second)
 
 
 def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
@@ -37,10 +31,7 @@ def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
     If @first and @second are the same object should return True
     In another case should return False
     """
-    if first is second:
-        return True
-    else:
-        return False
+    return first is second
 
 
 def multiple_ints(first_value: int, second_value: int) -> int:
@@ -90,11 +81,9 @@ def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
             print("Not valid input data")
         >>> "Not valid input data"
     """
-    first_value = int(first_value)
-    second_value = int(second_value)
-    if type(first_value * second_value) == int:
+    try:
         return int(first_value) * int(second_value)
-    else:
+    except ValueError:
         raise OurAwesomeException
 
 
@@ -114,10 +103,7 @@ def is_word_in_text(word: str, text: str) -> bool:
         >>> False
         
     """
-    if word in text:
-        return True
-    else:
-        return False
+    return word in text
 
 
 def some_loop_exercise() -> list:
@@ -125,12 +111,9 @@ def some_loop_exercise() -> list:
     Use loop to create list contains int values from 0 to 12 except 6 and 7
     """
     list = []
-    i = 0
-    while i <= 12:
-        list.append(i)
-        i += 1
-    list.remove(6)
-    list.remove(7)
+    for i in range(13):
+        if i not in (6, 7):
+            list.append(i)
     return list
 
 
@@ -143,29 +126,25 @@ def remove_from_list_all_negative_numbers(data: List[int]) -> list:
         remove_from_list_all_negative_numbers([1, 5, -7, 8, -1])
         >>> [1, 5, 8]
     """
-    new_lst = data[:]
-    for item in new_lst:
-        if item < 0:
-            data.remove(item)
-    return(data)
+    new_lst = []
+    for i in data:
+        if i >= 0:
+            new_lst.append(i)
+    return(new_lst)
 
 
 def alphabet() -> dict:
     """
-    Create dict where keys are alphabetic characters. And values are it number in alphabet
+    Create dict where keys are numbers accessing order starting from 1. And values are the symbols in alphabet order.
     Notes You could see the implementaion of this one in test, but create another one
     Examples:
         alphabet()
         >>> {"a": 1, "b": 2 ...}
     """
     dict = {}
-    keys = range(1, 26)
-    values = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
-            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", 
-            "x", "y", "z"]
-    for i, k in enumerate(values, 1):
-        dict[i] = k
-    return(dict)
+    for counter, symb in enumerate(string.ascii_lowercase, 1):
+        dict[counter] = symb
+    return dict
 
 
 def simple_sort(data: List[int]) -> List[list]:
@@ -178,9 +157,7 @@ def simple_sort(data: List[int]) -> List[list]:
 
     """
     for i in range(len(data)):
-        min = i
         for k in range(i + 1, len(data)):
-            if data[k] < data[min]:
-                min = k
-        data[min], data[i] = data[i], data[min]
+            if data[i] > data[k]:
+        data[i], data[k] = data[k], data[i]
     return data
